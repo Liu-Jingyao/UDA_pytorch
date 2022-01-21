ab6adf5f487d5d0c29a67094bab062e651ebfaed
柳景耀 <ljyayg@163.com>
2022-01-21 18:04:41 +0800

    继续修改配置为使用bert_medium

--- download.sh
+++ download.sh
@@ -16,6 +16,8 @@
 
 # **** download pretrained models ****
 #wget storage.googleapis.com/bert_models/2018_10_18/uncased_L-12_H-768_A-12.zip
+#unzip uncased_L-12_H-768_A-12.zip && rm uncased_L-12_H-768_A-12.zip
+#mv uncased_L-12_H-768_A-12 BERT_Base_Uncased
 wget https://storage.googleapis.com/bert_models/2020_02_20/uncased_L-8_H-512_A-8.zip
 
 unzip uncased_L-8_H-512_A-8.zip && rm uncased_L-8_H-512_A-8.zip
