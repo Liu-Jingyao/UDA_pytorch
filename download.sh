dfb9876244c9da861610d02a37f3a5b20e843573
柳景耀 <ljyayg@163.com>
2022-01-21 18:18:00 +0800

    修改download.sh文件适合bert-mediun

--- download.sh
+++ download.sh
@@ -19,9 +19,7 @@
 #unzip uncased_L-12_H-768_A-12.zip && rm uncased_L-12_H-768_A-12.zip
 #mv uncased_L-12_H-768_A-12 BERT_Base_Uncased
 wget https://storage.googleapis.com/bert_models/2020_02_20/uncased_L-8_H-512_A-8.zip
-
-unzip uncased_L-8_H-512_A-8.zip && rm uncased_L-8_H-512_A-8.zip
-mv uncased_L-8_H-512_A-8 BERT_Medium_Uncased
+unzip uncased_L-8_H-512_A-8.zip -d BERT_Medium_Uncased && rm uncased_L-8_H-512_A-8.zip
 
 # **** unzip data ****
 unzip data.zip && rm data.zip
