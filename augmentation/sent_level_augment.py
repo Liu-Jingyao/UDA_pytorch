8b4f56c4ea6e727f64b0802df86d74f02355a2d3
柳景耀 <ljyayg@163.com>
2022-01-29 12:22:53 +0800

    使用管道

--- augmentation/sent_level_augment.py
+++ augmentation/sent_level_augment.py
@@ -34,16 +34,6 @@ def back_translation(ori_lines, aug_ops, aug_copy_num, aug_batch_size, max_len):
     for i in tqdm(range(batch_num)):
         start = i * aug_batch_size
         end = min(start + aug_batch_size, len(ori_lines))
-        # translated_tokens = en_fr_model.generate(
-        #     **{k: v.cuda() for k, v in
-        #        en_fr_tokenizer(ori_lines[start:end], return_tensors="pt", padding=True, truncation=True, max_length=max_len).items()},
-        #     do_sample=True,
-        #     top_k=5,
-        #     top_p=0.70,
-        #     temperature=temp,
-        #     num_return_sequences=aug_copy_num
-        # )
-        # in_fr = [en_fr_tokenizer.decode(t, skip_special_tokens=True) for t in translated_tokens]
         in_fr = en_fr_translator(ori_lines[start:end])
         fr_lines.extend(in_fr)
     print("Translating French data back to English...")
@@ -51,15 +41,6 @@ def back_translation(ori_lines, aug_ops, aug_copy_num, aug_batch_size, max_len):
     for i in tqdm(range(batch_num)):
         start = i * aug_batch_size
         end = min(start + aug_batch_size, len(fr_lines))
-        # bt_tokens = fr_en_model.generate(
-        #     **{k: v.cuda() for k, v in fr_en_tokenizer(fr_lines[start:end], return_tensors="pt", padding=True, truncation=True, max_length=512).items()},
-        #     do_sample=True,
-        #     top_k=10,
-        #     top_p=0.95,
-        #     temperature=temp,
-        #     num_return_sequences=aug_copy_num
-        # )
-        # in_en = [fr_en_tokenizer.decode(t, skip_special_tokens=True) for t in bt_tokens]
         in_en = fr_en_translator(ori_lines[start:end])
         aug_lines.extend(in_en)
     return aug_lines
